6c3673aed2a01ba428571c0b8b9577a19df70d3b
acosst97 <duviel62@hotmail.com>
2025-05-09 10:32:34 -0500

    ajustes recuperacion correo

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -64,7 +64,7 @@ export class AuthService {
     this.intervalSubscription = interval(this.tokenCheckInterval).subscribe(() => {
       this.checkTokenExpiration();
     });
-  }
+  } 
 
   stopTokenExpirationTimer(): void {
     if (this.intervalSubscription) {
