59122716eb411ec7edee5097737793a14d7a76dc
harvin.acosta <harvin.acosta@novabase.com.co>
2025-09-29 08:27:02 -0500

    variabbles docker v1

--- src/app/components/nav-board/nav-board.component.ts
+++ src/app/components/nav-board/nav-board.component.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import * as CryptoJS from 'crypto-js';
+// import * as CryptoJS from 'crypto-js';
 import { AuthService } from '../../services/auth.service';
 import { LoginData } from '../../interfaces/loginRequest';
 const SECRET_KEY = 'tu_clave_secreta';
 @Component({
   selector: 'app-nav-board',
   standalone: true,
-  imports: [RouterLink,CommonModule,RouterLinkActive],
+  imports: [RouterLink, CommonModule, RouterLinkActive],
   templateUrl: './nav-board.component.html',
   styleUrl: './nav-board.component.scss'
 })
@@ -24,14 +24,14 @@ export class NavBoardComponent {
     this.usuario = this.authSrv.obtenerUsuario();
     console.log("usuario desencriptado navboard", this.usuario);
     this.roles = this.usuario?.roles || [];
-  this.validatePermiso();
+    this.validatePermiso();
   }
 
-  validatePermiso(){
-const esAdmin = this.tieneRol('ADMINISTRADOR');
-this.puedeVerUsuarios = esAdmin || this.tieneAlgunRol(['EMPLEADO', 'CLIENTE']);
-this.puedeVerVehiculosServicios = esAdmin || this.tieneAlgunRol(['EMPLEADO']);
-this.puedeVerReservaciones = esAdmin || this.tieneAlgunRol(['EMPLEADO', 'CLIENTE']);
+  validatePermiso() {
+    const esAdmin = this.tieneRol('ADMINISTRADOR');
+    this.puedeVerUsuarios = esAdmin || this.tieneAlgunRol(['EMPLEADO', 'CLIENTE']);
+    this.puedeVerVehiculosServicios = esAdmin || this.tieneAlgunRol(['EMPLEADO']);
+    this.puedeVerReservaciones = esAdmin || this.tieneAlgunRol(['EMPLEADO', 'CLIENTE']);
   }
 
   tieneRol(rol: string): boolean {
@@ -41,5 +41,5 @@ this.puedeVerReservaciones = esAdmin || this.tieneAlgunRol(['EMPLEADO', 'CLIENTE
   tieneAlgunRol(rolesPermitidos: string[]): boolean {
     return this.roles.some(rol => rolesPermitidos.includes(rol));
   }
-  constructor(private authSrv:AuthService){}
+  constructor(private authSrv: AuthService) { }
 }
